ee47107e329974b79dea9543fb5b7a018ba819ae
aaron <xch1523480@163.com>
2018-05-18 17:16:52 +0800

    commit

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -4,19 +4,26 @@ import $ from 'jquery'
 import { Router, Route, Link } from 'react-router'
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showNav:false
+    }
+  }
   render() {
     return (
       <div>
         <div id='mainNav'>
           <i id='main_logo'></i>
-          <ul id='main_links'>
+          <ul className='main_links'>
             <li className='active link_item'><a href="javascript:;">首页</a></li>
             <li className='link_item'><a href="javascript:;">简介</a></li>
             <li className='link_item'><a href="javascript:;">特点</a></li>
             <li className='link_item'><a href="javascript:;">技术</a></li>
             <li className='link_item'><a href="javascript:;">ROADMAP</a></li>
-            <li><a href="javascript:;">白皮书</a></li>
+            <li><a href="whiteBook2.5.pdf">白皮书</a></li>
           </ul>
+          <i id='nav_button'></i>
         </div>
         <div className='tab_panel' id='tab_panel1'>
           <i />
@@ -27,22 +34,22 @@ class Home extends Component {
             <h2>简介</h2>
             <i></i>
             <div></div>
-            <p>Magic Eye是一个基于物联网和区块链技术的3D数据生态系统。通过物联网化的3D智能硬件集群和搭建<br />基于区块链底层架构的数据分享平台，构建一个集3D数据生成、分享和消费的生态体系。</p>
+            <p>Magic Eye是一个基于物联网和区块链技术的3D数据生态系统。通过物联网化的3D智能硬件集群和搭建基于区块链底层架构的数据分享平台，构建一个集3D数据生成、分享和消费的生态体系。</p>
           </div>
           <div className='tab_panel' id='tab_panel3'>
             <h2>特点</h2>
             <i></i>
             <div className='tab_panel3_item1'>
-              <div className='left1'>物联网与区块链紧密结合，智能3D终端将通过完整的物联网解决方案接入Magic Eye生态系统。</div>
               <div className='right1'></div>
+              <div className='left1'>物联网与区块链紧密结合，智能3D终端将通过完整的物联网解决方案接入Magic Eye生态系统。</div>
             </div>
             <div className='tab_panel3_item2'>
               <div className='left2'></div>
               <div className='right2'>借助Magic Eye底层公链和分布式存储，生态系统参与者可以汇集各类3D影视、3D游戏、3DUGC等泛娱乐内容和3D模组等数据文件到区块链综合数据平台，且数据版权将得到保护。</div>
             </div>
             <div className='tab_panel3_item3'>
-              <div className='left3'>建立内容分发过程的价值传播机制，形成用户积极参与内容分享与传播的良好生态环境。</div>
               <div className='right3'></div>
+              <div className='left3'>建立内容分发过程的价值传播机制，形成用户积极参与内容分享与传播的良好生态环境。</div>
             </div>
           </div>
           <div className='tab_panel' id='tab_panel4'>
@@ -110,13 +117,15 @@ class Home extends Component {
   componentDidMount() {
     let tab_panels = $('.tab_panel')
     let link_items = $('.link_item')
+    let nav_button = $('#nav_button')
+    let main_links = $('.main_links')
     window.addEventListener('scroll', () => {
       let scrollTop = $(document).scrollTop();
       tab_panels.each(function (index, element) {
         let contentItem = $(this);
         let offsetTop = contentItem.offset().top;
         if (scrollTop > offsetTop - 250) {
-          $("#main_links").find(".active").removeClass("active");
+          $(".main_links").find(".active").removeClass("active");
           $(link_items[index]).addClass('active');
         }
       });
@@ -126,6 +135,16 @@ class Home extends Component {
       let index = link_items.index(this);
       $('body,html').animate({ scrollTop: $('.tab_panel:eq(' + index + ')').offset().top - 200 }, 200);
     });
+    nav_button.click( ()=> {
+      this.setState({
+        showNav:!this.state.showNav
+      })
+      if (this.state.showNav) {
+        main_links.addClass('showNav');
+      }else{
+        main_links.removeClass('showNav');
+      }
+    })
   }
 }
 
